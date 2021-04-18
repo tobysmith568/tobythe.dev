d1c544cbdbd9cdc5bf505d75dfd01c35f5273457
Toby Smith <tobysmith568@hotmail.co.uk>
2021-04-18 18:20:08 +0100

    Added a background image and basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { codeSlash, logoGithub } from "ionicons/icons";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = "tobythe-dev";
+export class AppComponent implements OnInit {
+  public websiteLogo: SafeUrl;
+  public gitHubLogo: SafeUrl;
+
+  constructor(private domSanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.websiteLogo = this.domSanitizer.bypassSecurityTrustUrl(codeSlash);
+    this.gitHubLogo = this.domSanitizer.bypassSecurityTrustUrl(logoGithub);
+  }
 }
