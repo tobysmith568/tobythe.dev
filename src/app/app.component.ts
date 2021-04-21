0c87ed2f32565c71743da95d3ac44f8fb52ea6c5
Toby Smith <tobysmith568@hotmail.co.uk>
2021-04-21 22:49:41 +0100

    Tweaked the wave and some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { codeSlash, logoGithub } from "ionicons/icons";
-import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-  public websiteLogo: SafeUrl;
-  public gitHubLogo: SafeUrl;
-
-  constructor(private domSanitizer: DomSanitizer) {}
-
-  ngOnInit(): void {
-    this.websiteLogo = this.domSanitizer.bypassSecurityTrustUrl(codeSlash);
-    this.gitHubLogo = this.domSanitizer.bypassSecurityTrustUrl(logoGithub);
-  }
-}
+export class AppComponent {}
